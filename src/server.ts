9ef1d41e8341f498abc0f70e0e54ae01d1909b46
FrancoisToureille <toureillefrancois@gmail.com>
2026-01-12 16:46:53 +0100

    ajout modele prof

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import * as YAML from 'yaml';
+import swaggerUi from 'swagger-ui-express';
+import addressController from './adapters/driving/addressController';
+import path from 'path';
+import * as fs from 'node:fs';
+
+const app = express();
+app.use(express.json());
+
+const file = fs.readFileSync('./openapi.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/addresses', addressController);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+  console.log(`Swagger docs at http://localhost:${port}/docs`);
+});
